6df5e80b091c55719dc6076feb8ad50191d093a1
Felix Mendez <pfmata01@gmail.com>
2015-09-05 16:13:28 -0600

    Responsive Login

--- pages/login.php
+++ pages/login.php
@@ -3,25 +3,20 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Login</title>
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
 	<link rel="stylesheet" href="../css/login.css">
-	<style>
-		body{
-			background:url("../assets/img/header_login.jpg");
-		}
-		.sombra{
-			text-shadow: black 0.1em 0.1em 0.1em;
-		}
-	</style>
 	<link rel="stylesheet" href="../font-awesome-4.4.0/css/font-awesome.min.css">
 </head>
 <body>
-	<form action="">
+	<form action="" >
 	  <h1><span class="fa fa-sign-in"></span><span class="sombra"> Login</span></h1>
+	  
 	  <button class="submit"><span class="fa fa-unlock-alt"></span></button>
 	  <span class="fa fa-user inputUserIcon"></span>
 	  <input type="text" class="user" placeholder="ursername"/>
 	  <span class="fa fa-key inputPassIcon"></span>
 	  <input type="password" class="pass"placeholder="password"/>
+	  <button class="submit ocultar"><span class="fa fa-unlock-alt"></span></button>
 	</form>
 	
 	<script src="../js/jquery-1.11.3.min.js"></script>
