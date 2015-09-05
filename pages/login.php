337ccc0dee4fdd6e0e9d6c450a452fa00ce2b73f
Felix Mendez <pfmata01@gmail.com>
2015-09-05 11:38:19 -0600

    DIseño Login
    Diseño de login

---
+++ pages/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login</title>
+	<link rel="stylesheet" href="../css/login.css">
+</head>
+<body>
+	<form action="">
+	  <h2><span class="entypo-login"></span> Login</h2>
+	  <button class="submit"><span class="entypo-lock"></span></button>
+	  <span class="entypo-user inputUserIcon"></span>
+	  <input type="text" class="user" placeholder="ursername"/>
+	  <span class="entypo-key inputPassIcon"></span>
+	  <input type="password" class="pass"placeholder="password"/>
+	</form>
+	
+	<script src="../js/jquery-1.11.3.min.js"></script>
+	<script src="../js/login.js"></script>
+</body>
+</html>
