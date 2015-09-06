c65607cacefb1bfd4be605c4c32f5002cd0cbfd6
Felix Mendez <pfmata01@gmail.com>
2015-09-06 00:28:41 -0600

    Validacion Login
    Validacion de usuario y contraseña vacios

--- js/login.js
+++ js/login.js
@@ -1,12 +1,96 @@
+/*--------------------------------------
+		DECLARACION DE VARIABLES
+-----------------------------------------*/
+var ejecutar=true;
+var user;
+var password;
 
-$(".user").focusin(function(){
-  $(".inputUserIcon").css("color", "#e74c3c");
-}).focusout(function(){
-  $(".inputUserIcon").css("color", "white");
-});
+/*--------------------------------------
+			Funcion de enfoques
+				en input
+-----------------------------------------*/
 
-$(".pass").focusin(function(){
-  $(".inputPassIcon").css("color", "#e74c3c");
-}).focusout(function(){
-  $(".inputPassIcon").css("color", "white");
+function focoinput(){
+	$("#user").focusin(function(){
+	  $(".inputUserIcon").css("color", "#e74c3c");
+	}).focusout(function(){
+	  $(".inputUserIcon").css("color", "white");
+	});
+
+	$("#pass").focusin(function(){
+	  $(".inputPassIcon").css("color", "#e74c3c");
+	}).focusout(function(){
+	  $(".inputPassIcon").css("color", "white");
+	});
+}
+
+/*--------------------------------------
+			Reset DE TEXT
+-----------------------------------------*/
+
+function resets(){
+	$(".MensajeError").fadeOut();
+	$("#spuser").css("color", "white");
+	$("#sppass").css("color", "white");
+	$("#user").removeClass("holder errorinput");
+	$("#spuser").removeClass("errorspan");
+	$("#pass").removeClass("holder errorinput");
+	$("#sppass").removeClass("errorspan");
+}
+
+/*--------------------------------------
+			LIMPIADO DE TEXT
+-----------------------------------------*/
+function limpiarInput(){
+	user=$("#user").val().trim();
+	password=$("#pass").val().trim();
+}
+
+
+
+
+$(document).ready(function(){
+
+	focoinput();
+
+	$("#submit").click(function(){
+		resets();
+		ejecutar=true;
+		limpiarInput();
+
+		if(user==""){
+			$('.MensajeError').text('Usuario y Contraseña Requeridos');
+			$(".MensajeError").fadeIn();
+			$("#user").addClass("holder errorinput");
+			$("#spuser").addClass("errorspan");
+			$("form").css("margin-top","100px");			
+			$('#user').val(user);			
+			ejecutar=false;
+		}
+
+		if(password==""){
+			$('.MensajeError').text('Usuario y Contraseña Requeridos');
+			$(".MensajeError").fadeIn();
+			$("#pass").addClass("holder errorinput");
+			$("#sppass").addClass("errorspan");
+			$("form").css("margin-top","100px");			
+			$('#pass').val(password);			
+			ejecutar=false;
+		}
+
+		if(ejecutar)
+		{
+			setTimeout(function(){
+			$("form").css("margin-top","230px");
+			},410);
+		}
+		
+	});
+
+	$("#submit2").click(function(){
+		$(".MensajeError").fadeIn();
+		$("form").css("margin-top","50px");
+	});
+	
+
 });
--- pages/login.php
+++ pages/login.php
@@ -6,17 +6,24 @@
 	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
 	<link rel="stylesheet" href="../css/login.css">
 	<link rel="stylesheet" href="../font-awesome-4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="../css/animate.css">
 </head>
 <body>
-	<form action="" >
+	
+	<div class="MensajeError animated rubberBand retraso-2">Error al ingresar los datos</div>
+
+	<form action="#" class="animated fadeInDown retraso-2">
 	  <h1><span class="fa fa-sign-in"></span><span class="sombra"> Login</span></h1>
 	  
-	  <button class="submit"><span class="fa fa-unlock-alt"></span></button>
-	  <span class="fa fa-user inputUserIcon"></span>
-	  <input type="text" class="user" placeholder="ursername"/>
-	  <span class="fa fa-key inputPassIcon"></span>
-	  <input type="password" class="pass"placeholder="password"/>
-	  <button class="submit ocultar"><span class="fa fa-unlock-alt"></span></button>
+	  <button id="submit"><span class="fa fa-unlock-alt"></span></button>
+
+	  <span class="fa fa-user inputUserIcon" id="spuser"></span>
+	  <input type="text" id="user"  placeholder="Usuario" autocomplete="off"/>
+
+	  <span class="fa fa-key inputPassIcon" id="sppass"></span>
+	  <input type="password" id="pass" placeholder="Contraseña" autocomplete="off"/>
+
+	  <button class="ocultar" id="submit2"><span class="fa fa-unlock-alt"></span></button>
 	</form>
 	
 	<script src="../js/jquery-1.11.3.min.js"></script>
