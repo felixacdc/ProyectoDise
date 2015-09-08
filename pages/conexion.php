bab88156d12adcb004171f81e2ef263ebea6cabb
Felix Mendez <pfmata01@gmail.com>
2015-09-07 23:56:28 -0600

    Clase Conexion
    Creacion de la clase de conexion a la base de datos

---
+++ pages/conexion.php
@@ -0,0 +1,15 @@
+<?php
+	
+class conexionbd extends mysqli{
+
+	public function __construct(){
+		parent::__construct('localhost','root','','edusoft');
+		$this->query("SET NAMES 'utf8';"); //verifica el lenguaje que se envia a la bd o se resive
+		$this->connect_errno ? die('Error en la conexion') : $x = 'Conectado';
+		echo $x;
+		unset($x); //elimina la variable de la memoria		
+	}	
+}
+
+
+?>
---
+++ pages/verificacionlogin.php
@@ -0,0 +1,8 @@
+<?php
+	
+	require('conexion.php');  //se utilisa como include pero la diferencia es que si hay error corta la ejecucion del programa
+	//y no permite ejecutar las lineas siguientes
+
+	$db = new conexionbd();
+
+?>
