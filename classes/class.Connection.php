7658513ff44f88b74af41c6a8d5b1f00fb4a6b74
felixacdc <pfmata01@gmail.com>
2015-09-20 23:20:35 -0600

    Conexion M1

--- classes/class.Connection.php
+++ classes/class.Connection.php
@@ -6,7 +6,7 @@ class conexionbd extends mysqli{
 		parent::__construct('localhost','root','','edusoft');
 		$this->query("SET NAMES 'utf8';"); //verifica el lenguaje que se envia a la bd o se resive
 		$this->connect_errno ? die('Error en la conexion') : $x = 'Conectado';
-		echo $x;
+		// echo $x;
 		unset($x); //elimina la variable de la memoria		
 	}	
 }
--- pages/verificacionlogin.php
+++ pages/verificacionlogin.php
@@ -1,6 +1,6 @@
 <?php
-	
-	require('conexion.php');  //se utilisa como include pero la diferencia es que si hay error corta la ejecucion del programa
+
+	require('../classes/class.Connection.php');  //se utilisa como include pero la diferencia es que si hay error corta la ejecucion del programa
 	//y no permite ejecutar las lineas siguientes
 
 	$db = new conexionbd();
