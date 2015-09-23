bcedaa44fc7a2a4aa927ac8f89f5c79dbad2b068
Felix Mendez <pfmata01@gmail.com>
2015-09-23 09:26:16 -0600

    gitignore
    Prueba

--- classes/class.Connection.php
+++ classes/class.Connection.php
@@ -9,10 +9,9 @@ class ConnectionClass extends mysqli{
 
 	public function __construct(){
 		parent::__construct($this->servidor, $this->user, $this->password, $this->db);
-		$this->query("SET NAMES 'utf8';"); //verifica el lenguaje que se envia a la bd o se resive
+		$this->query("SET NAMES 'utf8';");
 		$this->connect_errno ? die('Error en la conexion') : $x = 'Conectado';
-		// echo $x;
-		unset($x); //elimina la variable de la memoria
+		unset($x); 
 	}
 }
 
