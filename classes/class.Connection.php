4fa2de8d2382f55caa48c791ddb74f42e8eeb6ee
Felix Mendez <pfmata01@gmail.com>
2015-09-23 09:29:22 -0600

    gitignore
    Borrado

--- classes/class.Connection.php
+++ classes/class.Connection.php
@@ -11,7 +11,7 @@ class ConnectionClass extends mysqli{
 		parent::__construct($this->servidor, $this->user, $this->password, $this->db);
 		$this->query("SET NAMES 'utf8';");
 		$this->connect_errno ? die('Error en la conexion') : $x = 'Conectado';
-		unset($x); 
+		unset($x);
 	}
 }
 
